da1605a44a714b98135b23f3c53c07aff19e14ef
eminvergil <emintrhack@gmail.com>
2020-10-22 15:15:11 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="w-full flex flex-wrap justify-center">
+      <p className="flex self-center text-4xl font-bold tracking-wide">
+        hello from app
+      </p>
+    </div>
+  );
+}
